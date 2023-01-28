ebd25abcc68a752fdcc772254a4cb1791eb43019
hojinlee <infohojin@gmail.com>
2023-01-28 15:42:04 +0900

    laravel Provider

---
+++ src/JinyDatabaseServiceProvider.php
@@ -0,0 +1,42 @@
+<?php
+namespace Jiny\Database;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Blade;
+use Illuminate\View\Compilers\BladeCompiler;
+use Livewire\Livewire;
+
+class JinyDatabaseServiceProvider extends ServiceProvider
+{
+    private $package = "jinydatabase";
+    public function boot()
+    {
+        // 모듈: 라우트 설정
+        $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
+        $this->loadViewsFrom(__DIR__.'/../resources/views', $this->package);
+
+        // 데이터베이스
+        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+
+        // 설정파일 복사
+        $this->publishes([
+            __DIR__.'/../config/database.php' => config_path('jiny/database.php'),
+        ]);
+
+        $this->publishes([
+            __DIR__.'/../resources/actions/' => resource_path('actions')
+        ], 'database-actions');
+
+
+    }
+
+    public function register()
+    {
+        /* 라이브와이어 컴포넌트 등록 */
+        $this->app->afterResolving(BladeCompiler::class, function () {
+
+        });
+
+    }
+
+}
