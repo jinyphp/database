f207e835c0e7b42334dcfd8bd5c7a74e4c961ce9
hojinlee <infohojin@gmail.com>
2024-12-02 15:36:38 +0900

    provider and composer

--- JinyDatabaseServiceProvider.php
+++ JinyDatabaseServiceProvider.php
@@ -8,24 +8,24 @@ use Livewire\Livewire;
 
 class JinyDatabaseServiceProvider extends ServiceProvider
 {
-    private $package = "jinydatabase";
+    private $package = "jiny-dbms";
     public function boot()
     {
         // 모듈: 라우트 설정
-        $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
-        $this->loadViewsFrom(__DIR__.'/../resources/views', $this->package);
+        $this->loadRoutesFrom(__DIR__.'/routes/web.php');
+        $this->loadViewsFrom(__DIR__.'/resources/views', $this->package);
 
         // 데이터베이스
-        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+        $this->loadMigrationsFrom(__DIR__.'/database/migrations');
 
         // 설정파일 복사
         $this->publishes([
-            __DIR__.'/../config/database.php' => config_path('jiny/database.php'),
+            __DIR__.'/config/database.php' => config_path('jiny/database.php'),
         ]);
 
-        $this->publishes([
-            __DIR__.'/../resources/actions/' => resource_path('actions')
-        ], 'database-actions');
+        // $this->publishes([
+        //     __DIR__.'/../resources/actions/' => resource_path('actions')
+        // ], 'database-actions');
 
 
     }
